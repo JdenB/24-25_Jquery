3f9aa4c504f91add6bb8f87722186ab383d21d39
13263 <jayden.buckley@vtsd.com>
2025-01-13 09:14:44 -0500

    adjusted layout

--- Cart/js/Cart.js
+++ Cart/js/Cart.js
@@ -2,8 +2,11 @@ $(document).ready(function() {
 });
     function addProduct(){
         sessionStorage.setItem("price", "21.00")
+        sessionStorage.setItem("mKIQTY", "1")
+        sessionStorage.setItem("mKIname", "Pale_Rider")
+
         updateTotal();
     }
     function updateTotal(){
-        $("input#total").val(sessionStorage.getitem("price"));
+        $("input#total").val(sessionStorage.getItem("price"));
     }
